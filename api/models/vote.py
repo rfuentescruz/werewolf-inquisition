04bcba438c8bb05de4f0543d742949d5505d1957
Rolando Cruz <rolando.cruz21@gmail.com>
2016-11-02 12:41:41 +0800

    Allow nullable hut fields on votes
    NULL hut fields on votes simply means that the vote is still currently
    on the player and isn't placed on any of the huts.

--- api/models/vote.py
+++ api/models/vote.py
@@ -13,7 +13,10 @@ class Vote(models.Model):
         Player, on_delete=models.DO_NOTHING, related_name='votes'
     )
     hut = models.ForeignKey(
-        Hut, on_delete=models.DO_NOTHING, related_name='votes'
+        Hut,
+        on_delete=models.DO_NOTHING,
+        related_name='votes',
+        blank=True, null=True, default=None
     )
 
     time_created = models.DateTimeField(auto_now_add=True)
